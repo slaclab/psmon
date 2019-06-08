6f2089586f1c9271ab0388c8000942a470e98fc6
Daniel Damiani <ddamiani@slac.stanford.edu>
2019-06-07 18:03:41 -0700

    fix setup.py and remove scons stuff

--- setup.py
+++ setup.py
@@ -1,14 +1,27 @@
+import os
+import re
 from setuptools import setup
+
+
+def get_version(pkg):
+    """Scrap __version__  from __init__.py"""
+    vfilename = os.path.join(os.getcwd(), pkg, '__init__.py')
+    vfile = open(vfilename).read()
+    m = re.search(r'__version__ = (\S+)\n', vfile)
+    if m is None or len(m.groups()) != 1:
+        raise Exception("Cannot determine __version__ from init file: '%s'!" % vfilename)
+    version = m.group(1).strip('\'\"')
+    return version
 
 
 setup(
     name='psmon',
-    version='0.2.0',
+    version=get_version('psmon'),
     description='LCLS analysis monitoring',
     long_description='The psmom package is a remote data visualization tool used at LCLS for analysis monitoring',
     author='Daniel Damiani',
     author_email='ddamiani@slac.stanford.edu',
-    url='https://confluence.slac.stanford.edu/display/PSDM/psana+-+Python+Script+Analysis+Manual#psana-PythonScriptAnalysisManual-Real-timeOnlinePlotting/Monitoring',
+    url='https://confluence.slac.stanford.edu/display/PSDM/Visualization+Tools',
     packages=['psmon'],
     install_requires=[
         'numpy',
@@ -36,6 +49,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Scientific/Engineering :: Visualization',
         'Topic :: Utilities',
     ],
